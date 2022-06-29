952774dfab62df1edbda45e92425fa23884459d4
Mr-Raghu <sandeepraghuvanshi7898@gmail.com>
2022-06-29 20:22:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (<div >
+        <Header />
+        <div className="App">
+          <PathfindingVisualizer></PathfindingVisualizer>
+        </div>
+        <Footer />
     </div>
-  );
+
+    );
 }
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+import "./footer.css"
+
+export default class Footer extends Component{
+    render(){
+    return(
+        <div className="footer">
+            <div className="footer-hero">
+                <a href="https://github.com/Mr-Raghu">By Sandeep Raghuvanshi</a>
+                
+
+            </div>
+        </div>
+        
+    )
+    }
+
+}
